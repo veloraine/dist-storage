addadd2c0cfbd7355d23d5584b068319621c9da1
purylte <purylte@gmail.com>
2023-05-16 10:17:08 +0700

    caches ignore exception

--- dist_storage/settings.py
+++ dist_storage/settings.py
@@ -100,6 +100,7 @@ CACHES = {
         'LOCATION': 'redis://redis:6379/0',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'IGNORE_EXCEPTIONS': True,
         }
     }
 }
