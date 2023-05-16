c36a02c0bba1a642c82cd7291ac8049758425749
purylte <purylte@gmail.com>
2023-05-16 07:20:18 +0700

    fix typo

--- storage/dto/log_entry_payload.py
+++ storage/dto/log_entry_payload.py
@@ -2,7 +2,7 @@ class LogEntryPayload:
     def __init__(self, file, file_id, file_name):
         self.file = file
         self.file_id = file_id
-        self.file_name = file.name
+        self.file_name = file_name
 
     def to_dict(self):
         return {
